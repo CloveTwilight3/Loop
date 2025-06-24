1a3af83ef6423360041d9b212cd88f2e7167a1c7
Clove Twilight <admin@clovetwilight3.co.uk>
2025-06-24 20:40:46 +0100

    Updates

--- DiscordBot/Package.swift
+++ DiscordBot/Package.swift
@@ -4,18 +4,17 @@ import PackageDescription
 let package = Package(
     name: "DiscordBot",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/MahdiBM/DiscordBM.git", from: "1.0.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.19.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.19.0")
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
     ],
     targets: [
         .executableTarget(
             name: "DiscordBot",
             dependencies: [
-                .product(name: "DiscordBM", package: "DiscordBM"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "Logging", package: "swift-log")
             ]
